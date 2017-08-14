8eff6af3f00fc8da1359a610e3e976ca7b29c7d5
wwSchrader <wwschrader@gmail.com>
2017-08-14 14:48:13 -0700

    Add legend to stats block

--- src/PlayerStats.js
+++ src/PlayerStats.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./PlayerStats.css";
 
 class PlayerStats extends Component {
     constructor(props) {
@@ -17,11 +18,19 @@ class PlayerStats extends Component {
 
     render() {
         return(
-            <div>
-                <p>Health: {this.state.player.health}</p>
-                <p>Weapon: {this.state.player.weapon}</p>
-                <p>Experience: {this.state.player.experience}</p>
-                <p>Dungeon Level: {this.state.player.dungeonLevel}</p>
+            <div className="infoBlock">
+                <div className="statsLine">
+                    <span className="healthStats"></span><p>Health: {this.state.player.health}</p>
+                </div>
+                <div className="statsLine">
+                    <span className="weaponStats"></span><p>Weapon: {this.state.player.weapon}</p>
+                </div>
+                <div className="statsLine">
+                    <span className="experienceStats"></span><p className="experienceLine">Experience: {this.state.player.experience}</p>
+                </div>
+                <div className="statsLine">
+                    <span className="dungeonLevelStats"></span><p>Dungeon Level: {this.state.player.dungeonLevel}</p>
+                </div>
             </div>
         );
     }
